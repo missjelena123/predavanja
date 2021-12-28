a3d2f9de96810ac2bff20b27f9072aaf52c36540
Jelena Bogdanovic <miss.jelena.bogdanovic@gmail.com>
2021-12-28 19:20:33 +0100

    Zadatak1

---
+++ src/p_28_12_2021/Zzadatak1.java
@@ -0,0 +1,11 @@
+package p_28_12_2021;
+
+public class Zzadatak1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 for (int i = 0; i < 100; i++) {
+			   System.out.println( i +",");
+	}
+
+}
